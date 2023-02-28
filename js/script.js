ade1c51738fc555e780cc964b290fa3c5b5d6350
christianarca27 <christian.arca.mail@gmail.com>
2023-02-28 15:57:11 +0100

    feat: aggiunta la preview delle immagini a lato del carosello (bonus 2)

--- js/script.js
+++ js/script.js
@@ -12,23 +12,51 @@ console.log(topButtonEl);
 const bottomButtonEl = document.getElementById("bottom-button");
 console.log(bottomButtonEl);
 
+const previewsEl = document.getElementById("previews");
+console.log(previewsEl);
+
+for(let i = 0; i < pics.length; i++) {
+    let previewImgWrapperEl = document.createElement("div");
+    previewImgWrapperEl.classList.add("preview-img-wrapper");
+    previewImgWrapperEl.style.height = 100 / pics.length + "%";
+    previewImgWrapperEl.style.position = "relative";
+
+    previewsEl.append(previewImgWrapperEl);
+
+    let previewImgEl = document.createElement("img");
+    previewImgEl.classList.add("preview-img");
+    previewImgEl.src = pics[i];
+
+    previewImgWrapperEl.append(previewImgEl);
+}
+
 let index = 0;
 
 carouselImgEl.src = pics[index];
 
+previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
+
 topButtonEl.addEventListener("click", function() {
+    previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.remove("active");    
+
     index--;
     index += pics.length;
     index %= pics.length;
     console.log(index);
+    
+    previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
 
     carouselImgEl.src = pics[index];
 });
 
 bottomButtonEl.addEventListener("click", function() {
+    previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.remove("active"); 
+
     index++;
     index %= pics.length;
     console.log(index);
+    
+    previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
 
     carouselImgEl.src = pics[index];
 });
