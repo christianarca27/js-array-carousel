7b557106be659047320812df7f31129f5ede4131
christianarca27 <christian.arca.mail@gmail.com>
2023-02-28 16:55:09 +0100

    refactor: eliminati console log e aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
+// Inizializzo array di immagini da inserire nel carosello
 const pics = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
 
+// Leggo e salvo i riferimenti agli elementi presenti nel documento
 const carouselEl = document.getElementById("carousel");
-console.log(carouselEl);
-
 const carouselImgEl = document.getElementById("carousel-img");
-console.log(carouselImgEl);
-
 const topButtonEl = document.getElementById("top-button");
-console.log(topButtonEl);
-
 const bottomButtonEl = document.getElementById("bottom-button");
-console.log(bottomButtonEl);
-
 const previewsEl = document.getElementById("previews");
-console.log(previewsEl);
 
+// Riempio la previews con n-elementi (n = numero di immagini da inserire):
+//  - Creo un nuovo elemento div previewImgWrapperEl
+//  - Gli assegno la classe preview-img-wrapper
+//  - Gli do altezza pari al 100% / n
+//  - Gli assegno position = relative per fare da riferimento al before che scurisce la preview
+//  - Appendo il nuovo previewImgWrapperEl al previewsEl
+//  - Creo un nuovo elemento img previewImgEl
+//  - Gli assegno la classe preview-img
+//  - Gli collego la corretta immagine dall'array pics[]
+//  - Appendo il nuovo previewImgEl al previewImgWrapperEl
 for(let i = 0; i < pics.length; i++) {
     let previewImgWrapperEl = document.createElement("div");
     previewImgWrapperEl.classList.add("preview-img-wrapper");
@@ -30,31 +33,38 @@ for(let i = 0; i < pics.length; i++) {
     previewImgWrapperEl.append(previewImgEl);
 }
 
+// Inizializzo l'immagine da mostrare al caricamento della pagina e attivo la sua preview
 let index = 0;
-
 carouselImgEl.src = pics[index];
-
 previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
 
+// All'evento di click del tasto top:
+//  - Rimuovo la preview dell'immagine attuale come attiva
+//  - Decremento index (e gestisco eventuali overflow)
+//  - Assegno pics[index] come preview attiva
+//  - Inserisco pics[index] come immagine del carosello
 topButtonEl.addEventListener("click", function() {
     previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.remove("active");    
 
     index--;
     index += pics.length;
     index %= pics.length;
-    console.log(index);
     
     previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
 
     carouselImgEl.src = pics[index];
 });
 
+// All'evento di click del tasto bottom:
+//  - Rimuovo la preview dell'immagine attuale come attiva
+//  - Incremento index (e gestisco eventuali overflow)
+//  - Assegno pics[index] come preview attiva
+//  - Inserisco pics[index] come immagine del carosello
 bottomButtonEl.addEventListener("click", function() {
     previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.remove("active"); 
 
     index++;
     index %= pics.length;
-    console.log(index);
     
     previewsEl.querySelectorAll(".preview-img-wrapper").item(index).classList.add("active");
 
